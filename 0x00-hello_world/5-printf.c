36a7903f162fc8bf11428f8ce56bc8d2332949c1
Sbusiso <mshengussbusiso@gmail.com>
2023-08-10 23:21:15 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - writing a script that prints with printf
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
