a3306127af92c54a84e3b237cc9338860d5e9727
Sbusiso <mshengussbusiso@gmail.com>
2023-08-11 05:09:49 -0700

    variable type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,17 @@
  */
 int main(void)
 {
-int i;
-double d;
+int  i;
+long int b;
+long long int d;
 char c;
 float f;
 
-printf("Size of a char: %lu.\n byte(s)", (unsigned long)sizeof(c))
-printf("Size of a int: %lu.\n byte(s)", (unsigned long)sizeof(i))
-printf("Size of a long int: %lu.\n byte(s)", (unsigned long)sizeof(d))
-printf("Size of a long long int: %lu.\n byte(s)", (unsigned long)sizeof(dd))
-printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f))
+printf("Size of a char: %lu.\n byte(s)", (unsigned long)sizeof(c);)
+printf("Size of a int: %lu.\n byte(s)", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu.\n byte(s)", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu.\n byte(s)", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
 return (0);
 }
-}
+
