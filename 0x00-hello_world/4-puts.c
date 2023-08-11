051a48258776d160aeb2c9c2c3709593044d28ee
Sbusiso <mshengussbusiso@gmail.com>
2023-08-10 23:01:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - a program that runs on puts
+ *Return:0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
