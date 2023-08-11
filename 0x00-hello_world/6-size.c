63af16d7dad0afe920797253f9765dce4fcdbc01
Sbusiso <mshengussbusiso@gmail.com>
2023-08-11 05:25:15 -0700

    neew type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ long int b;
 long long int d;
 float f;
 
-printf("Size of a char: %lu.\n byte(s)", (unsigned long)sizeof(c);)
+printf("Size of a char: %lu.\n byte(s)", (unsigned long)sizeof(c));
 printf("Size of a int: %lu.\n byte(s)", (unsigned long)sizeof(i));
 printf("Size of a long int: %lu.\n byte(s)", (unsigned long)sizeof(b));
 printf("Size of a long long int: %lu.\n byte(s)", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
+printf("Size of a float: %lu.\n byte(s)", (unsigned long)sizeof(f));
 return (0);
 }
 
