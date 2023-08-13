927c9df86f60ad4d1e17f2b8281bfd9b2fa1de97
Sbusiso <mshengussbusiso@gmail.com>
2023-08-13 11:15:43 -0700

    quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints without printf or puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+write("and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 61);
+return (1);
+}
+
