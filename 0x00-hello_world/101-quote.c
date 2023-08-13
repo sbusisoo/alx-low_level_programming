74bb6786f9bb555aa5c6ff9bc73638677630896f
Sbusiso <mshengussbusiso@gmail.com>
2023-08-13 11:43:28 -0700

    quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - a program that prints without printf or puts
- * Return: 0 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 61);
+fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 return (1);
 }
 
