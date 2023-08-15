5a08497f014afc4191c8b44f14900b2f9cd49761
Sbusiso <mshengussbusiso@gmail.com>
2023-08-14 22:44:59 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the alphabet in low case
+ * Return: 0
+ */
+
+int main(void)
+{
+	char a;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
