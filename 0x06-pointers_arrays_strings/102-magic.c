3c7a54a28750081b39992988421c21b5dffa12b1
Sbusiso <mshengussbusiso@gmail.com>
2023-08-25 00:48:22 -0700

    stillll magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * main - a program that prints a[2] = 98
- * Return: void
  */
-void main(void)
-{
-	int n;
-	int a[5];
-	int *p;
 
-	a[2] = 1024;
-	p = &n;
+int main(void)
+{
+  int n;
+  int a[5];
+  int *p;
+
+  a[2] = 1024;
+  p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -20,8 +18,9 @@ void main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-*(p + 5) = 98;
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
+
